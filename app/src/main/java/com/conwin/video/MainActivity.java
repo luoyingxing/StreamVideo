3bb2f159ca4dc7abe35d0f82d6ff02bf2049ec1b
luoyingxing <luoyingxing@126.com>
2018-05-15 17:42:29 +0800

    refresh

--- app/src/main/java/com/conwin/video/MainActivity.java
+++ app/src/main/java/com/conwin/video/MainActivity.java
@@ -8,6 +8,8 @@ import com.conwin.video.jni.Test;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String videoURL = "http://test.jingyun.cn:27000/stream/read?flag=1&streamid=W545ee6ET7mfLimcg+Zv9A";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +17,19 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(new Test().stringFromJNI());
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
     }
 
 
