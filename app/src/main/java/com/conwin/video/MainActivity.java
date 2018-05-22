53a3819d2c1b03dc3508d2e17f9b98aa2ff94e1f
luoyingxing <luoyingxing@126.com>
2018-05-22 17:47:48 +0800

    test by byte

--- app/src/main/java/com/conwin/video/MainActivity.java
+++ app/src/main/java/com/conwin/video/MainActivity.java
@@ -21,6 +21,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -55,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
             super.handleMessage(msg);
             Bitmap bitmap = (Bitmap) msg.obj;
 
+            Log.w("-----bitmap-----> ", "" + bitmap);
+
             if (null != bitmap) {
                 imageView.setImageBitmap(bitmap);
             }
@@ -82,6 +85,8 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
+    private String regex = "------WebKitFormBoundaryIZDrYHwuf2VJdpHw";
+
     private String videoURL = "http://test.jingyun.cn:27000/stream/read?flag=0&from=2018-05-18%2020:37:20.0&streamid=";
 
     private void request() {
@@ -100,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
 
             conn.setRequestProperty("Connection", "Keep-Alive");
 //            conn.setRequestProperty("Connection", "zh-cn");
-            conn.setRequestProperty("Charsert", "UTF-8");
+//            conn.setRequestProperty("Charsert", "UTF-8");
 
 //            conn.setRequestProperty("Transfer-Encoding", "chunked");
 //            conn.setRequestProperty("Trailer", "finish-flag");
@@ -118,26 +123,259 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("---------Header--- ", key + " : " + map.get(key));
             }
 
-            Log.e("---------- ", "map: " + map.toString());
+//            Log.e("---------- ", "map: " + map.toString());
 
             //得到响应流
             InputStream inputStream = conn.getInputStream();
 
             int responseCode = conn.getResponseCode();
 
-            StringBuilder builder = new StringBuilder();
-
+//            StringBuilder cacheBuilder = new StringBuilder();
 
             if (responseCode == HttpURLConnection.HTTP_OK) {
 
-                byte[] data = new byte[1024];
-                StringBuffer sb = new StringBuffer();
+                byte[] read = new byte[1024];
 
-                while (inputStream.read(data) != -1) {
-                    String s = new String(data, "UTF-8");
-//                    sb.append(s);
+//                byte[] cache = new byte[1024 * 100];
 
-                    Log.d("-------- data --> ", s);
+                byte[] blockCache = new byte[0];
+
+//                StringBuffer cache = new StringBuffer();
+
+                byte[] regexByte = regex.getBytes();
+                int index = 0;
+
+                while (inputStream.read(read) != -1) {
+
+//                    Log.e("----inputStream------ ", new String(read, "UTF-8"));
+
+                    Log.d("----data block----", Arrays.toString(read));
+
+//                    String s = new String(data, "UTF-8");
+
+                    //累计到数组
+                    byte[] cur = blockCache;
+                    byte[] news = new byte[cur.length + read.length];
+
+                    if (blockCache.length == 0) {
+                        blockCache = read;
+                    } else {
+                        for (int i = cur.length; i < news.length; i++) {
+                            news[i] = read[i - cur.length];
+                        }
+
+                        blockCache = news;
+
+                    }
+
+
+                    boolean find = false;
+                    int flag = 0;
+
+                    //TODO 寻找分隔符
+                    for (int i = 0; i < blockCache.length; i++) {
+
+                        if (blockCache[i] == regexByte[0]) {
+                            int count = 0;
+
+                            for (int j = 0; j < regexByte.length; j++) {
+
+//                                Log.i("----for----", "i:" + i +
+//                                        " j:" + j +
+//                                        " regexByte.length:" + regexByte.length +
+//                                        " read.length:" + read.length);
+
+                                if (blockCache[i + j] == regexByte[j]) {
+                                    count++;
+                                }
+                            }
+
+                            if (count == regexByte.length) {
+                                //TODO find
+                                find = true;
+                                flag = i;
+                            }
+                        }
+                    }
+
+
+                    if (find) {
+
+                        byte[] cache = new byte[flag]; //块数据
+
+                        for (int i = 0; i < flag; i++) {
+                            cache[i] = blockCache[i];
+                        }
+
+                        //截掉前面一节
+                        byte[] splitCache = new byte[blockCache.length - cache.length];
+
+                        for (int i = 0; i < splitCache.length; i++) {
+                            splitCache[i] = blockCache[i + flag];
+                        }
+
+                        blockCache = splitCache;
+
+                        //TODO 块数据处理，寻找图片数据
+                        //判断换行符进行分割
+
+                        byte[] str = "\r\n\r\n".getBytes();
+
+                        if (cache.length >= str.length) {
+
+                            for (int k = 0; k < cache.length; k++) {
+                                int count = 0;
+
+                                if (cache[k] == str[0]) {
+
+                                    for (int p = 0; p < str.length; p++) {
+                                        if (cache[p + k] == str[p]) {
+                                            count++;
+                                        }
+                                    }
+                                }
+
+                                if (count == str.length) {
+
+                                    byte[] image = new byte[cache.length - k - str.length];
+
+                                    for (int w = 0; w < image.length; w++) {
+                                        image[w] = cache[w + k + str.length];
+                                    }
+
+                                    Message msg = handler.obtainMessage();
+                                    msg.obj = getBitmapFromByte(image);
+                                    handler.sendMessage(msg);
+                                }
+                            }
+
+                        }
+
+
+                    }
+
+
+//                    boolean find = false;
+//                    int flag = 0;
+//
+//                    //TODO 寻找分隔符
+//                    for (int i = 0; i < read.length; i++) {
+//                        flag = i;
+//                        int count = 0;
+//
+//                        if (read[i] == regexByte[0]) {
+//
+//                            for (int j = 0; j < regexLength; j++) {
+//
+//                                if (read[j] == regexByte[j]) {
+//                                    count++;
+//                                }
+//                            }
+//                        }
+//
+//                        if (count == regexLength) {
+//                            //TODO find
+//                            find = true;
+//                            index = i;
+//                        }
+//                    }
+//
+//
+//                    if (find) {
+//
+//                        for (int i = cache.length - 1; i < read.length - flag + cache.length; i++) {
+//                            cache[i] = (byte) (i);
+//                        }
+//
+//
+//                        //判断换行符进行分割
+//
+//                        byte[] str = "\r\n\r\n".getBytes();
+//                        boolean findSplit = false;
+//                        int split = 0;
+//
+//                        for (int k = 0; k < cache.length; k++) {
+//
+//                            int count = 0;
+//
+//                            if (cache[k] == str[0]) {
+//
+//                                for (int p = 0; p < str.length; p++) {
+//                                    if (cache[p] == str[p]) {
+//                                        count++;
+//                                    }
+//                                }
+//                            }
+//
+//                            if (count == str.length) {
+//                                split = k;
+//                                findSplit = true;
+//                            }
+//                        }
+//
+//
+//                        if (findSplit) {
+//
+//                            byte[] image = new byte[cache.length - split];
+//
+//                            for (int q = split; q < cache.length; q++) {
+//                                image[q - split] = cache[q];
+//                            }
+//
+//
+//                            Message msg = handler.obtainMessage();
+//                            msg.obj = getBitmapFromByte(image);
+//                            handler.sendMessage(msg);
+//
+//                        }
+//
+//
+//                    } else {
+//
+//                        for (int i = index; i < cache.length; i++) {
+//                            cache[i] = (byte) (i);
+//                        }
+//                    }
+
+
+//                    if (s.contains(regex)) {
+//                        String[] str = s.trim().split(regex);
+//
+//                        if (str.length > 0) {
+//                            cache.append(str[0]);
+//                        }
+//
+//                        //TODO 检查是否已满足一块数据
+//                        String blockData = cache.toString().trim();
+////                        Log.v("块数据", blockData);
+//
+//                        String[] blocks = blockData.split("\r\n\r\n");
+//
+//                        if (blocks.length > 1) {
+//                            String image = blocks[1].trim();
+//
+////                            Log.w("--block--header--", blocks[0]);
+////                            Log.d("--block--image--", blocks[1]);
+//
+//                            Message msg = handler.obtainMessage();
+//                            msg.obj = convertStringToIcon(image);
+//                            handler.sendMessage(msg);
+//                        }
+//
+//
+//                        //TODO 清空数据
+//                        cache.setLength(0);
+//
+//
+//                        if (str.length > 1) {
+//                            cache.append(str[1]);
+//                        }
+//
+//
+//                    } else {
+//                        cache.append(s);
+//                    }
+
 
 //                    Log.d("---------- ", "二进制数据: " + byte2hex(data));
 
@@ -205,11 +443,15 @@ public class MainActivity extends AppCompatActivity {
     public Bitmap convertStringToIcon(String st) {
         Bitmap bitmap;
         try {
-            byte[] bitmapArray;
-            bitmapArray = Base64.decode(st, Base64.DEFAULT);
+            byte[] bitmapArray = st.getBytes("UTF-8");
+
+//            Log.d("--Bitmap--", Arrays.toString(bitmapArray));
+
+//            bitmapArray = Base64.decode(st.trim(), Base64.DEFAULT);
             bitmap = BitmapFactory.decodeByteArray(bitmapArray, 0, bitmapArray.length);
             return bitmap;
         } catch (Exception e) {
+            e.printStackTrace();
             return null;
         }
     }
